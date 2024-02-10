2fc51b854f6e192b76bb7daf107b9bd14be6f3ee
Parth Date <dateparth6@gmail.com>
2024-02-10 19:17:30 +0530

    page.js changes

--- practice/src/app/page.js
+++ practice/src/app/page.js
@@ -1,4 +1,4 @@
-import { Counter } from "@/count/counter";
+
 import Image from "next/image";
 import styles from "./page.module.css";
 
@@ -6,7 +6,6 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <button>Increment</button>
-      <Counter />
       <button>Descrement</button>
     </main>
   );
